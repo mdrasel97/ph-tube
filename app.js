ee8e8260e5bc5a58eaab72600f2ef5c2b7a1232b
mdrasel97 <mollarasel972@gmail.com>
2025-03-12 14:45:52 +0600

    video section added

--- app.js
+++ app.js
@@ -1,42 +1,80 @@
-
 // const showLoader = () =>{
 //     const loaderBtn = document.getElementById('loader');
 //     loaderBtn.classList.remove('hidden')
 // }
 
-const categoryLoad = () =>{
-    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    .then(Response => Response.json())
-    .then(data =>{
-        categoryDisplay(data.categories)
-    })
-}
+const categoryLoad = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((Response) => Response.json())
+    .then((data) => {
+      categoryDisplay(data.categories);
+    });
+};
 
-const videoLoad = ()=>{
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
-    .then(Response =>Response.json())
-    .then(data=>{
-        videosDisplay(data)
-    })
-}
+const videoLoad = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((Response) => Response.json())
+    .then((data) => {
+      videosDisplay(data.videos);
+    });
+};
 
-const videosDisplay = ()=>{
+const videosDisplay = (videos) => {
+  // console.log(videos)
+  const videoContainer = document.getElementById("video-container");
+  for (let video of videos) {
+    // console.log(video)
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="card bg-base-100">
+                <figure class="relative">
+                  <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="" />
+                    <p class="bg-black text-white absolute px-3 rounded-md bottom-2 right-2">3hrs 56 min ago</p>
+                </figure>
+                <div class="px-0">
+                  <div class="flex gap-3 py-5 pl-2">
+                    <div class="avatar w-8 h-8">
+                        <div class="ring-primary ring-offset-base-100 rounded-full ring ring-offset-2">
+                          <img class="px-0 " src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                        </div>
+                      </div>
+                      <div class="space-y-2">
+                        <h2 class="text-2xl font-semibold"> amar sonar bangla</h2>
+                        <p class="flex items-center gap-1 text-gray-500">
+                            awlad hossain
+                            <img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt="">
+                        </p>
+                        <p class="text-gray-500">91K views</p>
+                      </div>
+                  </div>
+                  <div class="flex gap-3">
+                    
+                  </div>
+                </div>
+                <div class="text-center">
+                    <button class="btn btn-wide">Show More</button>
+                </div>
+              </div>
+            `;
 
-}
+            videoContainer.append(div)
+  }
+};
 
-const categoryDisplay = (categories)=>{
-    const categoryContainer = document.getElementById('category-container');
-    for(let category of categories){
-        const categoryDiv = document.createElement('div');
-        categoryDiv.innerHTML = `
+const categoryDisplay = (categories) => {
+  const categoryContainer = document.getElementById("category-container");
+  for (let category of categories) {
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
         <div>
             <button class="btn bg-gray-200 text-gray-500 hover:bg-red-600 hover:text-white">${category.category}</button>
         </div>
-        `
-        categoryContainer.appendChild(categoryDiv)
-    }
-}
+        `;
+    categoryContainer.appendChild(categoryDiv);
+  }
+};
 
-
-categoryLoad()
-videoLoad()
+categoryLoad();
+videoLoad();
